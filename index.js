a3777dcc827160ebc87efd86f3fe2fe3390017f2
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-02-13 03:26:26 +0530

    Sound Functionality added

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
-
-document.querySelector("button").addEventListener("click", handleClick)
-
-function handleClick(){
-  alert("I got Clicked")
-}
+//
+// document.querySelector("button").addEventListener("click", handleClick)
+//
+// function handleClick(){
+//   alert("I got Clicked")
+// }
 
 var numberofDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i<numberofDrumButtons; i++) {
 
+  // Adding EventListener to Each Button
+
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
-    alert("I got Clicked");
+    // alert("I got Clicked");
+    // var audio = new Audio('sounds/tom-1.mp3')
+    // audio.play();
+    //
+    // this.style.color = "#fff"
+
+    var buttonInnerHTML = this.innerHTML
+
+    switch (buttonInnerHTML) {
+      case "w":
+        var tom1 = new Audio('sounds/tom-1.mp3')
+        tom1.play();
+        break;
+
+      case "a":
+        var tom2 = new Audio('sounds/tom-2.mp3')
+        tom2.play();
+        break;
+
+      case "s":
+        var tom3 = new Audio('sounds/tom-3.mp3')
+        tom3.play();
+        break;
+
+      case "d":
+        var tom4 = new Audio('sounds/tom-4.mp3')
+        tom4.play();
+        break;
+
+      case "j":
+        var snare = new Audio('sounds/snare.mp3')
+        snare.play();
+        break;
+
+      case "k":
+        var crash = new Audio('sounds/crash.mp3')
+        crash.play();
+        break;
+
+      case "l":
+        var kick = new Audio('sounds/kick-bass.mp3')
+        kick.play();
+        break;
+
+      default: console.log(buttonInnerHTML)
+
+    }
 
   });
 
