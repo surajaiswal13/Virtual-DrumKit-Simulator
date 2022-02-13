a130fa6ab5a29d491479527c6db6ef6abd73de42
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-02-13 16:01:38 +0530

    Added Keyboard Syncing word Sound

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-//
-// document.querySelector("button").addEventListener("click", handleClick)
-//
-// function handleClick(){
-//   alert("I got Clicked")
-// }
+// Detecting Button Press
 
 var numberofDrumButtons = document.querySelectorAll(".drum").length;
 
@@ -19,48 +14,74 @@ for (var i = 0; i<numberofDrumButtons; i++) {
     //
     // this.style.color = "#fff"
 
-    var buttonInnerHTML = this.innerHTML
+      var buttonInnerHTML =  this.innerHTML
 
-    switch (buttonInnerHTML) {
-      case "w":
-        var tom1 = new Audio('sounds/tom-1.mp3')
-        tom1.play();
-        break;
-
-      case "a":
-        var tom2 = new Audio('sounds/tom-2.mp3')
-        tom2.play();
-        break;
-
-      case "s":
-        var tom3 = new Audio('sounds/tom-3.mp3')
-        tom3.play();
-        break;
-
-      case "d":
-        var tom4 = new Audio('sounds/tom-4.mp3')
-        tom4.play();
-        break;
-
-      case "j":
-        var snare = new Audio('sounds/snare.mp3')
-        snare.play();
-        break;
-
-      case "k":
-        var crash = new Audio('sounds/crash.mp3')
-        crash.play();
-        break;
-
-      case "l":
-        var kick = new Audio('sounds/kick-bass.mp3')
-        kick.play();
-        break;
-
-      default: console.log(buttonInnerHTML)
-
-    }
+      audioSyncWithCode(buttonInnerHTML)
 
   });
 
+}
+
+// Detecting Key Press
+
+// Adding EventListener to entire document
+
+document.addEventListener("keydown", function() {
+
+  audioSyncWithCode(event.key)
+
+})
+
+// Function for playing audio
+
+function audioSyncWithCode(keyPressed) {
+
+  switch (keyPressed) {
+    case "w":
+      var tom1 = new Audio('sounds/tom-1.mp3')
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3')
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3')
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3')
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3')
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/crash.mp3')
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3')
+      kick.play();
+      break;
+
+    default:
+      // if (buttonInnerHTML) {
+      //   console.log(buttonInnerHTML)
+      // }
+      // else if (event.key) {
+      //   console.log(event.key)
+      // }
+      // else {
+      //   console("wrong Key")
+      // }
+      console.log("Wrong Input")
+  }
 }
